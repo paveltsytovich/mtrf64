3ed9187857c643ce66258c8ceaa691d3ff37a35f
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-01-07 17:55:06 +0300

    Rename class to class Command

--- MTRF64Command.js
+++ MTRF64Command.js
@@ -1,6 +1,5 @@
-class MTRF64Adapter {
-    constructor(port=null) {
-        this.port = port;
+class MTRF64Command {
+    constructor() {
         this.startBit = 171;
         this.mode = 4;
         this.ctr = 0;
@@ -13,7 +12,7 @@ class MTRF64Adapter {
         this.crc = 175;
         this.stopBit = 172        
     }
-    buildCommand() {
+    buildPacket() {
         return [this.startBit,this.mode,this.ctr,0,this.ch,this.cmd,this.fmt,
         this.d[0],this.d[1],this.d[2],this.d[3],this.id[0],this.id[1],this.id[2],this.id[3],
         this._crc(),this.stopBit];  
@@ -25,4 +24,4 @@ class MTRF64Adapter {
     }
 }
 
-module.exports = MTRF64Adapter;
+module.exports = MTRF64Command;
---
+++ tests/MTRF64Command.test.js
@@ -0,0 +1,45 @@
+require('mocha');
+const chai = require('chai');
+
+
+
+const MTRF64Command = require('../MTRF64Command');
+const SerialPort = require('serialport/test');
+
+const devPath = "/dev/ttyUSB0";
+const assertArrays = require('chai-arrays');
+chai.use(assertArrays);
+chai.should();
+
+describe("Command elementary tests",() =>{
+    var port;
+    beforeEach(() => {
+       
+        const mockBinding = SerialPort.Binding;
+        mockBinding.createPort(devPath,{echo: false, record: false});
+        port = new SerialPort(devPath);
+        
+    });
+
+    it("Command should be have all properties",() => {
+       var command = new MTRF64Command();
+       command.should.have.property("startBit",171);
+       command.should.have.property("mode",4);
+       command.should.have.property("ctr").eq(0);
+       command.should.have.property("togl").eq(0);
+       command.should.have.property("ch").eq(0);
+       command.should.have.property("cmd").eq(0);
+       command.should.have.property("d").to.be.equalTo([0,0,0,0]);
+       command.should.have.property("id").to.be.equalTo([0,0,0,0]);
+       command.should.have.property("crc",175);
+       command.should.have.property("stopBit").eq(172);
+    });
+
+    it("Build Packet Should be correact byte array",() => {
+        var command = new MTRF64Command();
+        const packet = command.buildPacket()
+        packet.should.to.be.equalTo([171,4,0,0,0,0,0,0,0,0,0,0,0,0,0,175,172]);
+
+    });
+  
+});
