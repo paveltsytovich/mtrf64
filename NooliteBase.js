774b2dc87388252cf4ab689809d18ae90c13266d
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-01-08 18:06:49 +0300

    Start of develop NooliteBase class

---
+++ NooliteBase.js
@@ -0,0 +1,5 @@
+class NooliteBase {
+
+}
+
+module.exports = NooliteBase;
---
+++ tests/NooliteBase.test.js
@@ -0,0 +1,32 @@
+require('mocha');
+const chai = require('chai');
+chai.should();
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+const NooliteBase = require('../NooliteBase')
+
+describe("NooliteBase test suite",() => {
+    it("Base device have all need properties",() => {
+        assert.fail('Not implemented');
+    });
+   
+})
+
+describe("NooliteBase bind test suite",() => {
+    it("Base device success bind command",() => {
+        assert.fail('Not implemented');
+    });
+    it("Base device not execute bind should be Error",() => {
+        assert.fail('Not implemented');
+    });
+});
+
+describe("NooliteBase unbind test suite",() => {
+    it("Base device success unbind command",() => {
+        assert.fail('Not implemented');
+    });
+    it("Base device not execute unbind should be Error",() => {
+        assert.fail('Not implemented');
+    });
+});
