0f2d7037228f95883011a5958a0f77cadd86abd9
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-01-08 16:10:58 +0300

    Refactor test for Buffer in MTRF64Command class

--- tests/MTRF64Command.test.js
+++ tests/MTRF64Command.test.js
@@ -74,17 +74,21 @@ describe("Command elementary tests",() =>{
     });
     it("Command create from buffer should be ok",()=> {
         const receivePacket = [171,4,1,2,3,1,2,3,1,2,3,1,2,3,1,200,172];
-        var command = new MTRF64Command(Buffer.from(receivePacket));
-        command.should.have.property("startBit",171);
-        command.should.have.property("mode",4);
-        command.should.have.property("ctr").eq(1);
-        command.should.have.property("togl").eq(2);
-        command.should.have.property("ch").eq(3);
-        command.should.have.property("cmd").eq(1);
-        command.should.have.property("fmt").eq(2);
-        command.should.have.property("d").to.be.equalTo([3,1,2,3]);
-        command.should.have.property("id").to.be.equalTo([1,2,3,1]);
-        command.should.have.property("crc",200);
-        command.should.have.property("stopBit").eq(172);
+        var actualCommand = new MTRF64Command(Buffer.from(receivePacket));
+
+        var expectedCommand = {
+            startBit: 171,
+            mode: 4,
+            ctr: 1,
+            togl: 2,
+            ch: 3,
+            cmd: 1,
+            fmt: 2,
+            d: [3,1,2,3],
+            id: [1,2,3,1],
+            crc: 200,
+            stopBit: 172
+        };
+        expect(actualCommand).deep.equal(expectedCommand);
     })
 });
