9f3b14dfe4fb2c3a0f6c55a9e5c921dece9bf1e3
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-01-08 16:09:09 +0300

    Refactor few tests in MTRF64Command

--- tests/MTRF64Command.test.js
+++ tests/MTRF64Command.test.js
@@ -17,17 +17,21 @@ describe("Command elementary tests",() =>{
     
 
     it("Command should be have all properties",() => {
-       var command = new MTRF64Command();
-       command.should.have.property("startBit",171);
-       command.should.have.property("mode",4);
-       command.should.have.property("ctr").eq(0);
-       command.should.have.property("togl").eq(0);
-       command.should.have.property("ch").eq(0);
-       command.should.have.property("cmd").eq(0);
-       command.should.have.property("d").to.be.equalTo([0,0,0,0]);
-       command.should.have.property("id").to.be.equalTo([0,0,0,0]);
-       command.should.have.property("crc",175);
-       command.should.have.property("stopBit").eq(172);
+       var actualCommand = new MTRF64Command();
+       var expectedCommand = {
+           startBit:171,
+           mode: 4,
+           ctr: 0,
+           togl: 0,
+           ch: 0,
+           cmd: 0,
+           fmt: 0,
+           d: [0,0,0,0],
+           id: [0,0,0,0],
+           crc: 175,
+           stopBit:172
+       }
+       expect(actualCommand).deep.equal(expectedCommand);
     });
 
     it("Build Packet Should be correact byte array",() => {
@@ -38,18 +42,23 @@ describe("Command elementary tests",() =>{
     });
     it("Create command from byte array should be ok",() => {
         const receivePacket = [171,4,1,2,3,1,2,3,1,2,3,1,2,3,1,200,172];
-        var command = new MTRF64Command(receivePacket);
-        command.should.have.property("startBit",171);
-        command.should.have.property("mode",4);
-        command.should.have.property("ctr").eq(1);
-        command.should.have.property("togl").eq(2);
-        command.should.have.property("ch").eq(3);
-        command.should.have.property("cmd").eq(1);
-        command.should.have.property("fmt").eq(2);
-        command.should.have.property("d").to.be.equalTo([3,1,2,3]);
-        command.should.have.property("id").to.be.equalTo([1,2,3,1]);
-        command.should.have.property("crc",200);
-        command.should.have.property("stopBit").eq(172);
+        var actualCommand = new MTRF64Command(receivePacket);
+
+        var expectedCommand = {
+            startBit: 171,
+            mode: 4,
+            ctr: 1,
+            togl: 2,
+            ch: 3,
+            cmd: 1,
+            fmt: 2,
+            d: [3,1,2,3],
+            id: [1,2,3,1],
+            crc: 200,
+            stopBit: 172
+        };
+
+        expect(actualCommand).deep.equal(expectedCommand);
 
     });
     it("Create command from non-array packet should be error",()=> {
