d2f286b5511dc6bb8606fb1e50fb8c145b54faae
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-01-12 12:56:46 +0300

    Implement test for NooliteDevice

--- NooliteDevice.js
+++ NooliteDevice.js
@@ -2,6 +2,7 @@ class NooliteDevice {
     constructor(adapter,channel,mode = NooliteDevice.Mode.Noolite) {
         this._channel = channel;
         this._adapter = adapter;
+        this.OnCommand = null;
     }
     get channel () {
         return this._channel;
---
+++ tests/NooliteDevice.test.js
@@ -0,0 +1,16 @@
+"use strict"
+require('mocha');
+const chai = require('chai');
+chai.should();
+const expect = require('chai').expect;
+
+const NooliteDevice = require('../NooliteDevice');
+
+describe("NooliteDevice elementary test suite",() => {
+    it("NooliteDevice has all properties",() => {
+        const device = new NooliteDevice(null,5);
+        device.should.have.property("_channel");
+        device.should.have.property("_adapter");
+        device.should.have.property("OnCommand");
+    });
+});
