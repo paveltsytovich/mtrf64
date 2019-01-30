fddf3b78c347cc0993baab27ae303bb384d1e622
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-01-30 21:52:54 +0300

    Create tests for unbind command

--- tests/RemoteControlNooliteDevice.test.js
+++ tests/RemoteControlNooliteDevice.test.js
@@ -91,3 +91,72 @@ describe("RemoteControlNooliteDevice bind command", () => {
         actualStatus.should.false;
     })
 });
+
+describe("Unbind command",() => {
+    var mockBinding;
+    var port;
+    var controller;
+    beforeEach(() => {
+        mockBinding = SerialPort.Binding;
+        mockBinding.createPort(devPath,{echo: false, record: true,autoOpen: true});
+        port = new SerialPort(devPath);  
+        controller = new MTRF64Controller(port);
+    });
+    it("unbind command for Noolite mode should be ok",() => {
+        var actualCommand;
+        await(()=> {
+            return new Promise((resolve) => {
+                controller._onSend = (command) => {
+                    actualCommand = command;
+                }
+                port.on('open',() => {
+                    var status = device.unbind();
+                    resolve(status);
+                })   
+            })
+        })();
+        var expectedCommand = {
+            _startBit:171,
+            _mode: 1,
+            _ctr: 5,
+            _togl: 0,
+            _ch: 5,
+            _cmd: 0,
+            _fmt: 0,
+            _d: [0,0,0,0],
+            _id: [0,0,0,0],
+            _crc: 182,
+            _stopBit:172
+        }
+        expect(actualCommand).deep.equal(expectedCommand);
+    });
+
+    it("unbind command for NooliteF mode should be ok",() => {
+        var actualCommand;
+        await(()=> {
+            return new Promise((resolve) => {
+                controller._onSend = (command) => {
+                    actualCommand = command;
+                }
+                port.on('open',() => {
+                    var status = device.unbind();
+                    resolve(status);
+                })   
+            })
+        })();
+        var expectedCommand = {
+            _startBit:171,
+            _mode: 3,
+            _ctr: 5,
+            _togl: 0,
+            _ch: 5,
+            _cmd: 0,
+            _fmt: 0,
+            _d: [0,0,0,0],
+            _id: [0,0,0,0],
+            _crc: 184,
+            _stopBit:172
+        }
+        expect(actualCommand).deep.equal(expectedCommand);
+    })
+})
