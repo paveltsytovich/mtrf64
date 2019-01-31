fbf38b9a19983f05dc74e8a7f9890154b1c4ffdd
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-01-31 20:39:14 +0300

    Start work with Relay

--- RelayNooliteDevice.js
+++ RelayNooliteDevice.js
@@ -5,6 +5,12 @@ class RelayNooliteDevice extends NooliteDevice {
 
         super(adapter,channel,mode)
     }
+    bind() {
+        throw Error('Not implemented');
+    }
+    unbind() {
+        throw Error('Not implemented');
+    }
     turnOn() {
         throw Error('Not implemented');
     }
@@ -12,49 +18,49 @@ class RelayNooliteDevice extends NooliteDevice {
         throw Error('Not implemented');
     }
     brightDown() {
-
+        throw Error('Not implemented');
     }
     brightUp() {
-
+        throw Error('Not implemented');
     }
     setBrightness(brightness) {
-
+        throw Error('Not implemented');
     }
     loadPreset() {
-
+        throw Error('Not implemented');
     }
     savePreset() {
-
+        throw Error('Not implemented');
     }
     stopReq() {
-
+        throw Error('Not implemented');
     }
     brightStepDown(step) {
-
+        throw Error('Not implemented');
     }
     brightStepUp(step) {
-
+        throw Error('Not implemented');
     }
     brightReq(direction,speed) {
-
+        throw Error('Not implemented');
     }
     rollColour() {
-
+        throw Error('Not implemented');
     }
     switchColour() {
-
+        throw Error('Not implemented');
     }
     switchMode() {
-
+        throw Error('Not implemented');
     }
     speedModeBack() {
-
+        throw Error('Not implemented');
     }
     temporaryOn(time) {
-
+        throw Error('Not implemented');
     }
     readState() {
-
+        throw Error('Not implemented');
     }
 }
 
