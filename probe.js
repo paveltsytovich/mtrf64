f2c579d0b03a4600e2833f6f7416e79e77291f82
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-02-01 20:19:23 +0300

    Create probe for Relay bind command

--- probe.js
+++ probe.js
@@ -5,7 +5,9 @@ const parser = port.pipe(new SerialPort.parsers.ByteLength({length: 17}));
 
 const MTRF64Controller = require('./MTRF64Controller');
 const RemoteControl = require('./RemoteControl');
+const Relay = require('./Relay');
 
+let controller = new MTRF64Controller(port,parser);
 
 class DoorSensor extends RemoteControl {
     constructor(controller,channel) {
@@ -18,11 +20,21 @@ class DoorSensor extends RemoteControl {
         console.log("door is closed");
     }
 }
+function DoorSensorProbe() {
 
-let controller = new MTRF64Controller(port,parser);
-
-
-let device = new DoorSensor(controller,1);
+    let device = new DoorSensor(controller,1);
 
 //device.bind();
-controller.register(device);
+    controller.register(device);
+}
+
+function RelayBindProbe() {
+    let device = new Relay(controller,1,Relay.Mode.Noolite);
+    device.bind();
+   // port.close();
+}
+
+port.on('open', () => {
+    RelayBindProbe();
+})
+
