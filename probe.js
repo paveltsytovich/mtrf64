a710f45f823d393f07e26213d463d51565998b1c
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-02-05 18:23:16 +0300

    Probe for setBigthness

--- probe.js
+++ probe.js
@@ -48,7 +48,7 @@ async function ElementaryCommandProbe() {
 }
 async function BrightnessProbe() {
     let device = new Relay(controller,3,Relay.Mode.Noolite);
-    console.log(await device.setBrightness(0.2));
+    console.log(await device.setBrightness(0.1));
     port.close();
     
 }
