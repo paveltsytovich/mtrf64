fc998149f55f76b42236b6b6d85434b8082e99ee
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-02-07 12:24:45 +0300

    Add doc for Remote Control

--- RemoteControl.js
+++ RemoteControl.js
@@ -1,9 +1,24 @@
+/**
+ * Implement RemoteControl class for Noolite-F device
+ * @copyright Pavel Tsytovich, 2019
+ */
 const AbstractRemoteControl = require('./AbstractRemoteControl');
 
 class RemoteControl extends AbstractRemoteControl {
+    /**
+     * Constructor
+     * @param {MTRF64Controller} controller - controller for manage device
+     * @param {number} channel - channel of device
+     * @param {NooliteDevice.Mode} mode - Noolite protocol mode 
+     */
     constructor(controller, channel, mode) {
         super(controller, channel, mode);
     }
+    /**
+     * Method for receive command handler. Do not use this method directly
+     * 
+     * @param {MTRF64Command} command 
+     */
     onCommand(command) {
         
         super.onCommand(command);
@@ -58,38 +73,87 @@ class RemoteControl extends AbstractRemoteControl {
                 break;
         }
     }
+    /**
+     * Handle for `Turn On` command from remote control device
+     */
     onTurnOn() {
     }
+    /**
+     * Handle for `Turn Off` command from remote control device
+     */
     onTurnOff() {
     }
+    /**
+     * Handle for `Bright Down` command from remote control device
+     */
     onBrightDown() {
     }
+    /**
+     * Handle `Bright Up` command from remote control device
+     */
     onBrightUp() {
     }
-    onSetBrightness(brightness) {
-    }
+    
+    /**
+     * Handle `Load Scenario` command from remote control device
+     */
     onExecuteScenario() {
     }
+    /**
+     * Handle `Save Scenario` command from remote control device
+     */
     onSaveScenario() {
     }
+    /**
+     * Handle `Stop Req` command from remote control device
+     */
     onStopReq() {
     }
+    /**
+     * Handle `Roll Colour` command from remote control device
+     */
     onRollColour() {
     }
+    /**
+     * Handle `Switch Colour` command from remote control device
+     */
     onSwitchColour() {
     }
+    /**
+     * Handle `Switch Mode` command from remote control device
+     */
     onSwitchMode() {
     }
+    /**
+     * Handle `SpeedBack Mode` command from remote control device
+     */
     onSpeedBackMode() {
     }
+    /**
+     * Handle `Send State` command from remote control device
+     * @param {Array} data - Data array with device state
+     */
     onSendState(data) {
     }
+    /**
+     * Handle `Switch` command from remote control device
+     */
     onSwitch() {
     }
+    /**
+     * Handle `Bright Back` command from remote control device
+     */
     onBrightBack() {
     }
+    /**
+     * Handle `Low Battery` command from remote control device
+     */
     onLowBattery() {
     }
+    /**
+     * Handle `Sens TempHumi` command from remote control device
+     * @param {Array} data - Data array with device state
+     */
     onSensTempHumi(data) {
     }
 }
