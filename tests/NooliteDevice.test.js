0fc5b8a28300971445cd2de1a7968f3f843df503
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2019-03-09 23:30:17 +0300

    Change un NooliteDevice tests

--- tests/NooliteDevice.test.js
+++ tests/NooliteDevice.test.js
@@ -8,15 +8,16 @@ const SerialPort = require('serialport/test');
 
 const devPath = "/dev/ttyUSB112";
 
+const MTRF64Driver = require('../');
 
-const NooliteDevice = require('../NooliteDevice');
+//const NooliteDevice = require('../NooliteDevice');
 
-const MTRF64Command = require('../MTRF64Command');
-const MTRF64Adapter = require('../MTRF64Adapter');
+//const MTRF64Command = require('../MTRF64Command');
+//const MTRF64Adapter = require('../MTRF64Adapter');
 
 describe("NooliteDevice elementary test suite",() => {
     it("NooliteDevice has all properties",() => {
-        const device = new NooliteDevice(null,5);
+        const device = new MTRF64Driver.NooliteDevice(null,5);
         device.should.have.property("_channel");
         device.should.have.property("_controller");
         device.should.have.property("onCommand");
